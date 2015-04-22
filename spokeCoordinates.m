fde5e684069ad583aff01b149bd563c08761d74c
Carl Mosbech <s134783@student.dtu.dk>
2015-04-22 17:24:22 +0200

    Test

--- spokeCoordinates.m
+++ spokeCoordinates.m
@@ -13,6 +13,7 @@ for n=1:nSpokes %Change sign for different side of hub
     x1Spoke(n) = x1Spoke(n)*sign;
     sign = sign*(-1);
 end
+%Does this even work?
 x2Spoke = zeros(1,nSpokes); 
 y1Spoke = rHub.*sin(linspace(0,2*pi,nSpokes+1));
 y1Spoke(nSpokes+1) = [];
